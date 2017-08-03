b9529b49325dde3de254a5f77203d076447a7319
Jaeger <JaegerCode@gmail.com>
2017-08-03 18:47:17 +0800

    ok

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,8 +8,8 @@ if [ ! -e /var/www/html/index.php ];  then
         mkdir -p /user-files/superuser/dl
         wget https://gist.githubusercontent.com/jae-jae/9b09a9bf90b733aed896713f56c8a1f2/raw/install-aria2-ui.sh 
         bash install-aria2-ui.sh
-        rm -R /var/www/html/dl
-        ln -s /user-files/superuser/dl /var/www/html/
+        #rm -R /var/www/html/dl
+        ln -s /user-files/superuser/dl /var/www/html/dl/files
         chown -R www-data:www-data /var/www/html
         chown -R www-data:www-data /user-files
         /wait-for-it.sh db:3306 -t 120 -- /import-db.sh
